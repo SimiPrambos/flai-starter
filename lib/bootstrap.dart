9e63575fb79a87976ede88d838cfbb8771e7aa38
Irwan Hermawan <rwnhermawan@gmail.com>
2026-05-13 14:58:05 +0700

    feat: add TalkerRiverpodLogger observer (non-production only)

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:talker_flutter/talker_flutter.dart';
+import 'package:talker_riverpod_logger/talker_riverpod_logger.dart';
 import 'package:template_vgv_app/core/logging/talker_provider.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
@@ -18,6 +20,9 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
     ProviderScope(
       overrides: [
         talkerProvider.overrideWithValue(talker),
+      ],
+      observers: [
+        if (!kReleaseMode) TalkerRiverpodObserver(talker: talker),
       ],
       child: await builder(),
     ),
